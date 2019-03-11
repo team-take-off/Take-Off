b2dd68b6a210f44f7a4626a25d539e1a802fbf81
Sharmarke Duale <sduale01@gmail.com>
2019-03-11 13:16:10 -0500

    setup employees saga file

---
+++ src/redux/sagas/employeesSaga.js
@@ -0,0 +1,53 @@
+import { put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+
+function* fetchEmployees(action) {
+    try {
+        const serverResponse = yield axios.get(`api/admin/employees`);
+        yield put({type: 'SET_EMPLOYEES', payload: serverResponse.data})
+    } catch (error) {
+        console.log('Error in axios GET:', error);
+    }
+}
+
+function* deleteEmployee(action) {
+    try {
+
+    } catch (error) {
+        console.log('Error in DELETE:', error);
+    }
+}
+
+
+function* addEmployee(action) {
+    try {
+
+    } catch (error) {
+        console.log('Error in DELETE:', error);
+    }
+}
+
+function* deactivateEmployee(action) {
+    try {
+
+    } catch (error) {
+        console.log('Error in :', error);
+    }
+}
+
+function* activateEmployee(action) {
+    try {
+
+    } catch (error) {
+        console.log('Error in :', error);
+    }
+}
+function* employeesSaga() {
+  yield takeLatest('FETCH_EMPLOYEES', fetchEmployees);
+  yield takeLatest('DELETE_EMPLOYEE', deleteEmployee);
+  yield takeLatest('ADD_EMPLOYEE', addEmployee);
+  yield takeLatest('DEACTIVATE_EMPLOYEE', deactivateEmployee);
+  yield takeLatest('ACTIVATE_EMPLOYEE', activateEmployee);
+}
+
+export default employeesSaga;
