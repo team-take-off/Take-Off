27c0391fd0daf0a5a63ae1ad548ed95bdf229aff
mes32 <stockman.mike@gmail.com>
2019-03-11 13:47:05 -0500

    Setup client-side routing (HashRouter) in App.js. Also add links to Nav bar.

---
+++ src/components/AdminCalendarPage/AdminCalendarPage.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class AdminCalendarPage extends Component {
+
+    // Show this component on the DOM
+    render() {
+        return (
+            <div>
+                <p>[ AdminCalendarPage ]</p>
+            </div>
+        );
+    }
+}
+
+export default AdminCalendarPage;
