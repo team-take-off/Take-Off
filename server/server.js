762a1a74d486ba35734e2353199e33ffc9392630
Maxwell Todd <mtmaxtodd@gmail.com>
2019-03-11 13:19:19 -0500

    Added the employeeRequest file to the router folder. The file is connected to the server file.

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const employeeRequestRouter = require('./routes/employeeRequest.router')
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -24,7 +25,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
-
+app.use('/api/employee/request', employeeRequestRouter);
 // Serve static files
 app.use(express.static('build'));
 
