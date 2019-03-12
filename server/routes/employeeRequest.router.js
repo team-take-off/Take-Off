b21ac33ed04b24bd04cf74d32c59998d0d86604c
mes32 <stockman.mike@gmail.com>
2019-03-11 23:51:54 -0500

    Build route POST /api/employee/request/ and test with Postman.

--- server/routes/employeeRequest.router.js
+++ server/routes/employeeRequest.router.js
@@ -29,26 +29,47 @@ router.get('/', (req, res) => {
 // Route POST /api/employee/request
 // User adds requested time-off to the database
 router.post('/', (req, res) => {
-    // if (req.isAuthenticated()) {
-    //     const queryText = `
-    //     SELECT
-    //         "time_off_request".* 
-    //         FROM "employee" 
-    //         JOIN "batch_of_requests" ON "employee"."id" = "batch_of_requests"."employee_id"
-    //         JOIN "time_off_request" ON "batch_of_requests"."id" = "time_off_request"."batch_of_requests_id"
-    //         WHERE "employee"."id" = $1;
-    //     `;
-    //     pool.query(queryText, [req.user.id]).then((queryResponse) => {
-    //         res.send(queryResponse.rows);
-    //     }).catch((queryError) => {
-    //         const errorMessage = `SQL error using POST /api/employee/request, ${queryError}`;
-    //         console.log(errorMessage);
-    //         res.sendStatus(500);
-    //     });
-    // } else {
-    //     res.sendStatus(403);
-    // }
-    res.sendStatus(200);
+    if (req.isAuthenticated()) {
+        const userID = req.user.id;
+        const typeID = req.body.requestType;
+        const requestedDates = req.body.requestedDates;
+        (async () => {
+            const client = await pool.connect();
+            try {
+                await client.query('BEGIN');
+                const insertComparisonText = `
+                INSERT INTO "batch_of_requests"
+                    ("employee_id", "type_id")
+                VALUES
+                    ($1, $2)
+                RETURNING id;
+                `;
+                const { rows } = await client.query(insertComparisonText, [userID, typeID]);
+                const batchID = rows[0].id;
+                for (let request of requestedDates) {
+                    const insertDateText = `
+                    INSERT INTO "time_off_request"
+	                    ("date", "batch_of_requests_id", "hours")
+                    VALUES
+	                    ($1, $2, $3);
+                    `;
+                    await client.query(insertDateText, [request.date, batchID, request.hours]);
+                }
+                await client.query('COMMIT');
+                res.sendStatus(201);
+            } catch (error) {
+                await client.query('ROLLBACK');
+                res.sendStatus(500);
+                throw error;
+            } finally {
+                client.release();
+            }
+        })().catch((error) => {
+            console.error(error.stack);
+        });
+    } else {
+        res.sendStatus(403);
+    }
 });
 
 module.exports = router;
