2fa67aa7be78db5086c888427a688633b7922bea
mes32 <stockman.mike@gmail.com>
2019-03-12 15:43:33 -0500

    Create RequestCard component.

--- src/components/RequestCard/RequestCard.js
+++ src/components/RequestCard/RequestCard.js
@@ -1,12 +1,141 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+
+// TODO: Remove or replace eventually
+const tempStyle = {
+    backgroundColor: '#ddd'
+};
+
+/* EXAMPLE INPUT
+
+const requestedDates = [
+    { date: '2019-05-02', hours: 8 },
+    { date: '2019-04-03', hours: 8 },
+    { date: '2019-05-05', hours: 8 },
+    { date: '2019-04-02', hours: 8 },
+    { date: '2019-04-01', hours: 4 },
+    { date: '2019-05-01', hours: 8 },
+];
+const conflicts = [
+    {
+        name: 'Sharmarke',
+        approved: true,
+        dates: [
+            { date: '2019-04-01' },
+            { date: '2019-04-02' },
+            { date: '2019-04-03' }
+        ]
+    },
+    {
+        name: 'Bode',
+        approved: false,
+        dates: [
+            { date: '2019-04-01' },
+            { date: '2019-04-02' }
+        ]
+    }
+];
+
+<RequestCard name="Michael" requestedDates={requestedDates} conflicts={conflicts} onApprove={this.onApprove} onDeny={this.onDeny} onCancel={this.onCancel} />
+
+*/
 
 class RequestCard extends Component {
+
+    formatAsDateRange = (datesArray) => {
+        // Read in an array of objects with the attribute 'date:' and convert
+        // to an array of class 'moment' from Moment.js.
+        let momentArray = [];
+        for (let element of datesArray) {
+            momentArray.push(moment(element.date, "YYYY-MM-DD"));
+        }
+
+        // Sort the array of moments
+        momentArray.sort(function (left, right) {
+            return left.diff(right);
+        });
+
+        // Build a human readable output string and condense all contiguous 
+        // dates into date ranges such as 'April 1, 2019 to April 3, 2019'.
+        let outputString = momentArray[0].format('LL');
+        for (let i = 1; i < momentArray.length; i++) {
+            if (i === momentArray.length - 1) {
+                outputString += ` to ${momentArray[i].format('LL')}`;
+            } else if (momentArray[i + 1].diff(momentArray[i], 'days') > 1) {
+                outputString += ` to ${momentArray[i].format('LL')} and ${momentArray[i+1].format('LL')}`;
+                i += 1;
+            }
+        }
+        return outputString;
+    }
+
+    // Renders an unordered list of conflicts with this request. Only applies if
+    //  an array of conflicts was  sent via props.
+    renderConflicts = () => {
+        if (this.props.conflicts && this.props.conflicts.length > 0) {
+            return (
+                <div>
+                    <h5>Conflicts:</h5>
+                    <ul>
+                        {this.props.conflicts.map(
+                            (conflict, index) => {
+                                const name = conflict.name;
+                                const dateRanges = this.formatAsDateRange(conflict.dates);
+                                const approved = conflict.approved ? '(Approved)' : '(Pending)';
+                                return (<li key={index}>{name} - {dateRanges} - {approved}</li>);
+                            }
+                        )}
+                    </ul>
+                </div>
+            );
+        }
+    }
+
+    // Renders a 'Approve' button. Only applies if an 'onApprove' function was 
+    // sent via props.
+    renderApproveButton = () => {
+        if (this.props.onApprove) {
+            return (
+                <button onClick={this.props.onApprove}>
+                    Approve
+                </button>
+            );
+        }
+    }
+
+    // Renders a 'Deny' button. Only applies if an 'onDeny' function was 
+    // sent via props.
+    renderDenyButton = () => {
+        if (this.props.onDeny) {
+            return (
+                <button onClick={this.props.onDeny}>
+                    Deny 
+                </button>
+            );
+        }
+    }
+
+    // Renders a 'Cancel' button. Only applies if an 'onCancel' function was 
+    // sent via props.
+    renderCancelButton = () => {
+        if (this.props.onCancel) {
+            return (
+                <button onClick={this.props.onCancel}>
+                    Cancel Request
+                </button>
+            );
+        }
+    }
 
     // Show this component on the DOM
     render() {
         return (
-            <div>
-                <p>[ RequestCard ]</p>
+            <div style={tempStyle}>
+                <h4>{this.props.name} - {this.formatAsDateRange(this.props.requestedDates)}</h4>
+                {this.renderConflicts()}
+                {this.renderApproveButton()}
+                {this.renderDenyButton()}
+                {this.renderCancelButton()}
             </div>
         );
     }
