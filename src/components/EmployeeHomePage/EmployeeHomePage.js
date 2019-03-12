5c06750af3f32b0dbdafc6b9741ab679bf93cb74
Maxwell Todd <mtmaxtodd@gmail.com>
2019-03-12 14:09:42 -0500

    Imported redux connect to the employee home page file and connected the file to the redux store. I now get the user object from the reduxstore.

--- src/components/EmployeeHomePage/EmployeeHomePage.js
+++ src/components/EmployeeHomePage/EmployeeHomePage.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
 class EmployeeHomePage extends Component {
     
     // Show this component on the DOM
     render() {
         return (
             <div>
+                {JSON.stringify(this.props.user.first_name)}
+                <h1>Welcome, {this.props.user.first_name}</h1>
                 <p>[ EmployeeHomePage ]</p>
             </div>
         );
     }
 }
 
-export default EmployeeHomePage;
+const mapStateToProps = state => ({
+    user: state.user,
+});
+
+// this allows us to use <App /> in index.js
+export default connect(mapStateToProps)(EmployeeHomePage);
