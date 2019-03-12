a4bac6bd4db99b360db934c3d5e42e12c87446bd
Sharmarke Duale <sduale01@gmail.com>
2019-03-12 13:53:38 -0500

    update user status admin

--- server/routes/adminAddTime.router.js
+++ server/routes/adminAddTime.router.js
@@ -7,21 +7,26 @@ router.get('/', (req, res) => {
 
 });
 
-router.put('/:id', (req, res) => {
-    // if (req.isAuthenticated()) {
-        const queryText = `
-            UPDATE "accrued_time" SET "sick_time" = "sick_time" + 1 
-            WHERE EXTRACT(YEAR FROM "year") = EXTRACT(YEAR FROM NOW()) 
-            AND "employee_id" = $1;`;
-        pool.query(queryText, [req.params.id])
-        .then(response =>{res.sendStatus(200)})
-        .catch(error => {
-            console.log('error in PUT', error);
-            res.sendStatus(500);
-        })
-    // }else {
-    //     res.sendStatus(403);
-    // }
+router.post('/:id', (req, res) => {
+    
+    if (req.isAuthenticated()) {
+        console.log('req paramas', req.params);
+    console.log('req user', req.user);
+    console.log('req body', req.body);
+        // const queryText = `
+        //     INSERT INTO "accrued_time" 
+        //         ("year", ${req.body.id == 1 ? "sick_time" : "vacation_time"}, "employee_id")
+        //     VALUES (')`
+            
+        // pool.query(queryText, [req.params.id])
+        // .then(response =>{res.sendStatus(200)})
+        // .catch(error => {
+        //     console.log('error in PUT', error);
+        //     res.sendStatus(500);
+        // })
+    }else {
+        res.sendStatus(403);
+    }
 });
 
 module.exports = router;
