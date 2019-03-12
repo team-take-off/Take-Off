ad1641f4d04b35903c53d6d10960bb6ffcaf8f7e
Sharmarke Duale <sduale01@gmail.com>
2019-03-12 00:00:39 -0500

    add partial query text to add time admin router

--- server/routes/adminAddTime.router.js
+++ server/routes/adminAddTime.router.js
@@ -7,8 +7,15 @@ router.get('/', (req, res) => {
 
 });
 
-router.post('/', (req, res) => {
-
+router.put('/', (req, res) => {
+    if (req.isAuthenticated()) {
+        const queryText = `
+            UPDATE "accrued_time" SET "sick_time" = "sick_time" + 1 
+            WHERE EXTRACT(YEAR FROM "year") = EXTRACT(YEAR FROM NOW()) 
+            AND "id" = $1;`;
+    }else {
+        res.sendStatus(403);
+    }
 });
 
 module.exports = router;
