a6984612f7b2e0ef7da14d8a60b92b91780ebabd
Bode Falade <bode.falade@outlook.com>
2019-03-12 15:35:47 -0500

    add GET roue for authenticated and authorized employee to get user info.

--- server/routes/employeeUserInfo.router.js
+++ server/routes/employeeUserInfo.router.js
@@ -3,9 +3,25 @@ const pool = require('../modules/pool');
 const router = express.Router();
 
 
-router.get('/', (req, res) => {
+// router.get('/', (req, res) => {
+//     if(req.isAuthenticated()){
+//         pool.query(`SELECT "employee"."first_name" AS "First Name", 
+//                     "employee"."last_name" AS "Last Name", "employee"."company_employee_id" AS "ID", 
+//                     "employee"."start_date" AS "Employee Start Date", 
+//                     "employee"."email" AS "Employee Email",
+//                     "employee"."is_active" AS "Time Off Approved Status"
+//                     FROM "employee" WHERE "id" = $1;`, [req.user.id])
+//                     .then((result)=> {
+//                         res.send(result.rows);
+//                     }).catch((error) => {
+//                         console.log('Error in GET route for user: ', error);
+//                         res.sendStatus(500);
+//                     });
+//     } else {
+//         res.sendStatus(403);
+//     }
 
-});
+// });
 
 router.post('/', (req, res) => {
 
