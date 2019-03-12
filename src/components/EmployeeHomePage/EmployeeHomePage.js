8c02946866b1673700c9da483c3c157c0e6fcc44
Maxwell Todd <mtmaxtodd@gmail.com>
2019-03-12 15:02:20 -0500

    added form elements to the employee home page and got necessary info from this.props.user. Clicking on the buttons will send the user to a page to request time off for vacation or for sick and safe leave. Corrected a typo on the server file.

--- src/components/EmployeeHomePage/EmployeeHomePage.js
+++ src/components/EmployeeHomePage/EmployeeHomePage.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+// import axios from 'axios';
+
 class EmployeeHomePage extends Component {
-    
+    constructor(){
+        super()
+        this.state={
+            sick_time:'',
+            vacation:'',
+        }
+    }
+//     getEmployee = (event)=> {
+//         axios({
+//         method:'GET',
+//         url:`/api/employee/userinfo:${this.props.user.id}`,
+//     }).then((response)=>{
+//         this.setState({
+//             vacation:response.vacation,
+//             sick_time:response.sick_time,
+//         })
+//     })
+// }
+
+vacationRequest = (event) =>{
+    this.props.history.push('/request_vacation')
+}
+
+sickRequest = (event) => {
+    this.props.history.push('/request_sick')
+}
     // Show this component on the DOM
     render() {
         return (
             <div>
-                {JSON.stringify(this.props.user.first_name)}
+                {/* {JSON.stringify(this.props.user)} */}
                 <h1>Welcome, {this.props.user.first_name}</h1>
-                <p>[ EmployeeHomePage ]</p>
+                <h2>Vacation Time: {this.props.user.vacation_hours}</h2>
+                <button onClick={this.vacationRequest}>Request Vacation</button>
+                <h2>Sick and Safe Time: {this.props.user.sick_hours}</h2>
+                <button onClick={this.sickRequest}>Request Sick and Safe</button>
             </div>
         );
     }
