ee8c8553edee142560a95e7892a283b26bd4f476
Sharmarke Duale <sduale01@gmail.com>
2019-03-12 14:40:20 -0500

    add columns to employee table

--- server/routes/adminAddTime.router.js
+++ server/routes/adminAddTime.router.js
@@ -10,20 +10,17 @@ router.get('/', (req, res) => {
 router.post('/:id', (req, res) => {
     
     if (req.isAuthenticated()) {
-        console.log('req paramas', req.params);
-    console.log('req user', req.user);
-    console.log('req body', req.body);
-        // const queryText = `
-        //     INSERT INTO "accrued_time" 
-        //         ("year", ${req.body.id == 1 ? "sick_time" : "vacation_time"}, "employee_id")
-        //     VALUES (')`
+        const queryText = `
+            INSERT INTO "accrued_time" 
+                ("sick_time", "vacation_time", "employee_id")
+            VALUES ($1, $2, $3)`
             
-        // pool.query(queryText, [req.params.id])
-        // .then(response =>{res.sendStatus(200)})
-        // .catch(error => {
-        //     console.log('error in PUT', error);
-        //     res.sendStatus(500);
-        // })
+        pool.query(queryText, [,,,])
+        .then(response =>{res.sendStatus(200)})
+        .catch(error => {
+            console.log('error in PUT', error);
+            res.sendStatus(500);
+        })
     }else {
         res.sendStatus(403);
     }
