e8a1fbb883896aaed35d14971167f509308ce646
Sharmarke Duale <sduale01@gmail.com>
2019-03-12 00:18:57 -0500

    update sick time routes added

--- server/routes/adminAddTime.router.js
+++ server/routes/adminAddTime.router.js
@@ -7,15 +7,21 @@ router.get('/', (req, res) => {
 
 });
 
-router.put('/', (req, res) => {
-    if (req.isAuthenticated()) {
+router.put('/:id', (req, res) => {
+    // if (req.isAuthenticated()) {
         const queryText = `
             UPDATE "accrued_time" SET "sick_time" = "sick_time" + 1 
             WHERE EXTRACT(YEAR FROM "year") = EXTRACT(YEAR FROM NOW()) 
-            AND "id" = $1;`;
-    }else {
-        res.sendStatus(403);
-    }
+            AND "employee_id" = $1;`;
+        pool.query(queryText, [req.params.id])
+        .then(response =>{res.sendStatus(200)})
+        .catch(error => {
+            console.log('error in PUT', error);
+            res.sendStatus(500);
+        })
+    // }else {
+    //     res.sendStatus(403);
+    // }
 });
 
 module.exports = router;
