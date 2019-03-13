bd7c434fbf51dc9ce6d8fe6de433cec916c00622
Maxwell Todd <mtmaxtodd@gmail.com>
2019-03-13 10:56:47 -0500

    Changed the display of the vacation and sick hours to display as days. Worked on the vacation request form and added a child component for it to add and remove days.

--- src/components/VacationRequestPage/VacationRequestPage.js
+++ src/components/VacationRequestPage/VacationRequestPage.js
@@ -1,15 +1,75 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import VacationRequestPageItem from './VacationRequestPageItem';
+// import axios from 'axios';
 
 class VacationRequestPage extends Component {
 
+    constructor() {
+
+        super()
+        this.state={
+            requests:[],
+            requestForm:[],
+        }
+    }
+
+    componentDidMount() {
+        this.addUserInfo();
+        
+    }
+
+    addUserInfo = (event) => {
+        const action = { type: 'SET_USER_INFO', payload: this.props.reduxStore.user }
+        this.props.dispatch(action)
+    }
+
+    addDay = (event) => {
+
+        
+        let itemToAdd = <input type="date" />
+        this.setState(prevState => ({
+            requestForm: [...prevState.requestForm, itemToAdd]
+        }))
+
+        
+    }
+
+
+
+     removeDay = (day) =>{
+         
+        this.setState({
+            requestForm: this.state.requestForm.filter((_, i) => i !== day)
+        });
+        
+        
+    }
+    
     // Show this component on the DOM
     render() {
+        console.log('array', this.state.requestForm);
+        // console.log(this.state.requestForm);
+        // let firstDay = <div><button onClick={this.subtractDay}>-</button><input type="date" /></div>
         return (
             <div>
-                <p>[ VacationRequestPage ]</p>
+                <h2>Vacation Time: {(parseFloat(this.props.reduxStore.userInfo.vacation_hours) / 8)} Days</h2>
+                <div><button onClick={this.subtractDay}>-</button><input type="date" /></div>
+                {/* {firstDay} */}
+                {this.state.requestForm.map((item,i) => {
+                    return <VacationRequestPageItem removeDay={this.removeDay} item={item} i={i}requestForm={this.state.requestForm}/>
+                })}
+            <button onClick={this.addDay}>+</button>
+            <br></br>
+            <button>Submit</button>
             </div>
         );
     }
 }
 
-export default VacationRequestPage;
+const mapStateToProps = reduxStore => ({
+    reduxStore
+});
+
+// this allows us to use <App /> in index.js
+export default connect(mapStateToProps)(VacationRequestPage);
