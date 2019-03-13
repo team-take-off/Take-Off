f6bbe4c3212fe25d3651f590d4ecb899772071c2
Maxwell Todd <mtmaxtodd@gmail.com>
2019-03-13 17:43:32 -0500

    added radio buttons to the request form to determine if a full or half day is being requested. Deleted unnecessary files from the RequestForm folder.

--- src/components/RequestForm/RequestFormRow/RequestFormRow.js
+++ src/components/RequestForm/RequestFormRow/RequestFormRow.js
@@ -5,16 +5,19 @@ class RequestFromRow extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            date: this.props.date,
+            date: this.props.request.date,
+            hours: this.props.request.hours,
         }
     }
 
     // Update the value of the date when the component updates
     componentDidUpdate(prevProps, prevState) {
-        if (prevProps.date !== this.props.date) {
+        if (prevProps.request.date !== this.props.request.date ||
+            prevProps.request.hours !== this.props.request.hours) {
             this.setState({
                 ...this.state,
-                date: this.props.date
+                date: this.props.request.date,
+                hours: this.props.request.hours,
             });
         }
     }
@@ -47,14 +50,31 @@ class RequestFromRow extends Component {
         };
         this.props.dispatch(action);
     }
+    
+    setHours = (event) => {
+        this.setState({
+            ...this.state,
+            hours: event.target.value
+        });
+        const action = {
+            type: 'SET_VACATION_REQUEST',
+            payload: {
+                index: this.props.index,
+                hours: event.target.value
+            }
+        };
+        this.props.dispatch(action);
+    }
 
     // Show this component on the DOM
     render() {
-        console.log('in render() for RequestFormRow ', );
+        console.log('in render() for RequestFormRow ');
         return (
             <div>
                 <button onClick={this.deleteRow}>-</button>
                 <input onChange={this.setDate} type="date" value={this.state.date} />
+                <input onChange={this.setHours} type="radio" name="full" value={8} />Full Day
+                <input onChange={this.setHours} type="radio" name="half" value={4} />Half Day
             </div>
         );
     }
--- src/components/VacationRequestPage/VacationRequestPage.js
+++ src/components/VacationRequestPage/VacationRequestPage.js
@@ -1,46 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import RequestForm from '../RequestForm/RequestForm';
-// import axios from 'axios';
 
 class VacationRequestPage extends Component {
 
     componentDidMount() {
-        this.addUserInfo();
-        
-    }
-
-    addUserInfo = (event) => {
         const action = { type: 'SET_USER_INFO', payload: this.props.reduxStore.user }
         this.props.dispatch(action)
     }
 
-    addDay = (event) => {
 
-        
-        let itemToAdd = <input type="date" />
-        this.setState(prevState => ({
-            requestForm: [...prevState.requestForm, itemToAdd]
-        }))
-
-        
-    }
-
-
-
-     removeDay = (day) =>{
-         
-        // this.setState({
-        //     requestForm: this.state.requestForm.filter((_, i) => i !== day)
-        // });
-        
-        
-    }
-    
-    // Show this component on the DOM
     render() {
-        // console.log(this.state.requestForm);
-        // let firstDay = <div><button onClick={this.subtractDay}>-</button><input type="date" /></div>
         return (
             <div>
                 <h2>Vacation Time: {(parseFloat(this.props.reduxStore.userInfo.vacation_hours) / 8)} Days</h2>
