9c02fb1cabaf5bedb56f0e52c6cdd4ac8ffc6a17
mes32 <stockman.mike@gmail.com>
2019-03-13 10:59:05 -0500

    Stashing changes to AdminAddEmployeePage.

--- src/components/AdminAddEmployeePage/AdminAddEmployeePage.js
+++ src/components/AdminAddEmployeePage/AdminAddEmployeePage.js
@@ -2,11 +2,37 @@ import React, { Component } from 'react';
 
 class AdminAddEmployeePage extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            first_name: '',
+            last_name: '',
+            username: '',
+            start_date: ''
+        };
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            ...this.state,
+            [event.target.name]: event.target.value
+        });
+        console.log(this.state);
+    }
+
+    submit = (event) => {
+        event.preventDefault();
+    }
+
     // Show this component on the DOM
     render() {
         return (
             <div>
-                <p>[ AdminAddEmployeePage ]</p>
+                <h2>Add Employee</h2>
+                <form onSubmit={this.submit}>
+                    <input onChange={this.handleChange} name="first_name" placeholder="First Name" type="text" />
+                    <input type="submit" value="Submit" />
+                </form>
             </div>
         );
     }
