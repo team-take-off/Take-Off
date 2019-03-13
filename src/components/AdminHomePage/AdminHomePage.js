8faed476479676fb39f0e33ed485a25715a54277
Sharmarke Duale <sduale01@gmail.com>
2019-03-13 16:04:46 -0500

    connect store to props

--- src/components/AdminHomePage/AdminHomePage.js
+++ src/components/AdminHomePage/AdminHomePage.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
 
 class AdminHomePage extends Component {
-
     // Show this component on the DOM
     render() {
         return (
             <div>
                 <h2>Pending Requests</h2>
+                {JSON.stringify(this.props.requests)}
             </div>
         );
     }
 }
-
-export default AdminHomePage;
+const mapStateToProps = (state) => ({
+    requests: state.requests
+});
+export default connect(mapStateToProps)(AdminHomePage);
