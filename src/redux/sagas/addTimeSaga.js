5725ed8cb6a8bc9dc6b71a51aff1138b018927db
Sharmarke Duale <sduale01@gmail.com>
2019-03-14 15:12:33 -0500

    add sick day to employee table and accrued time table

--- src/redux/sagas/addTimeSaga.js
+++ src/redux/sagas/addTimeSaga.js
@@ -5,6 +5,7 @@ function* addSickDay(action) {
     try {
         yield axios.post(`/api/admin/addtime/sick/${action.payload}`);
         yield axios.put(`/api/admin/addtime/sick/${action.payload}`);
+        yield put({type: 'FETCH_EMPLOYEES'});
     } catch (error) {
         console.log('Error in axios sick POST:', error);
     }
