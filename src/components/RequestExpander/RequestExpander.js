0b9fa47d948a991d9ae692ea7394224dea7f714f
mes32 <stockman.mike@gmail.com>
2019-03-14 16:39:32 -0500

    Filter for unique batches in RequestExpanderCollection.

--- src/components/RequestExpander/RequestExpander.js
+++ src/components/RequestExpander/RequestExpander.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 
+import RequestCard from '../RequestCard/RequestCard';
+
 class RequestExpander extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: this.props.open
+        }
+    }
+
+    renderTab = () => {
+        if (this.state.open) {
+            return <span>[ - ]</span>;
+        } else {
+            return <span>[ + ]</span>;
+        }
+    }
 
     // Show this component on the DOM
     render() {
         return (
             <div>
-                <p>[ RequestExpander ] [+]</p>
+                {this.renderTab()}
             </div>
         );
     }
--- src/components/RequestExpanderCollection/RequestExpanderCollection.js
+++ src/components/RequestExpanderCollection/RequestExpanderCollection.js
@@ -5,11 +5,45 @@ import moment from 'moment';
 class RequestExpanderCollection extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            pendingRequests: [],
-            approvedRequests: [],
-            pastRequests: []
+        this.state = this.getRequestArrays();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (prevProps.requests !== this.props.requests) {
+            this.setState(this.getRequestArrays());
+            const uniqueBatchIDs = this.findUniqueBatches(this.props.requests);
+            console.log(uniqueBatchIDs);
+        }
+    }
+
+    getRequestArrays = () => {
+        const requests = this.props.requests;
+        const uniqueBatches = this.filterUniqueBatches(requests);
+        let pendingRequests = [];
+        let approvedRequests = [];
+        let pastRequests = [];
+        
+        for (let batch of uniqueBatches) {
+            const id = batch.id;
+        }
+
+        return {
+            pendingRequests: pendingRequests,
+            approvedRequests: approvedRequests,
+            pastRequests: pastRequests
         };
+    }
+
+    filterUniqueBatches = (requests) => {
+        if (requests) {
+            return requests.filter((requestItem, index, uniqueArray) => {
+                if (uniqueArray.indexOf(requestItem) === index) {
+                    return requestItem;
+                }
+            }); 
+        } else {
+            return [];
+        }
     }
 
     groupBatchIds = () => {
@@ -28,9 +62,9 @@ class RequestExpanderCollection extends Component {
         return (
             <div>
                 {JSON.stringify(this.props.requests)}
-                <RequestExpander />
-                <RequestExpander />
-                <RequestExpander />
+                <RequestExpander open={true} requests={this.state.pendingRequests} />
+                <RequestExpander open={true} requests={this.state.approvedRequests} />
+                <RequestExpander open={false} requests={this.state.pastRequests} />
             </div>
         );
     }
