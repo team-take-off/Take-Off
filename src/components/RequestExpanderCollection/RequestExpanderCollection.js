60985479b3c5c8b369e47f8035bc1120b0e43bfe
mes32 <stockman.mike@gmail.com>
2019-03-15 08:43:31 -0500

    Clean up code in RequestExpanderCollection.

--- src/components/RequestExpanderCollection/RequestExpanderCollection.js
+++ src/components/RequestExpanderCollection/RequestExpanderCollection.js
@@ -8,12 +8,15 @@ class RequestExpanderCollection extends Component {
         this.state = this.getRequestArrays();
     }
 
+    // If the array of input requests changes. Regenerate the sorted output 
+    // arrays.
     componentDidUpdate(prevProps, prevState) {
         if (prevProps.requests !== this.props.requests) {
             this.setState(this.getRequestArrays());
         }
     }
 
+    // Sorts requests into arrays for pending, approved, and past requests
     getRequestArrays = () => {
         const currentTime = moment();
         const requests = this.props.requests;
@@ -23,24 +26,15 @@ class RequestExpanderCollection extends Component {
         let pastRequests = [];
         
         for (let id of batchIDs) {
+            // Sort requests into batches based on their 'batch_of_requests_id'
             const requestBatchArray = requests.filter(
-                request => parseInt(request.batch_of_requests_id) === parseInt(id)
+                request => request.batch_of_requests_id === id
             );
 
+            // Sort batches into the output arrays
             if (requestBatchArray.length > 0) {
-                // Read in an array of objects with the attribute 'date:' and convert
-                // to an array of class 'moment' from Moment.js.
-                let momentArray = [];
-                for (let element of requestBatchArray) {
-                    momentArray.push(moment(element.date, 'YYYY-MM-DD'));
-                }
-
-                // Sort the array of moments
-                momentArray.sort((left, right) => {
-                    return left.diff(right);
-                });
-
-                if (currentTime < momentArray[momentArray.length - 1]) {
+                const lastTimeInBatch = this.getLastMoment(requestBatchArray);
+                if (currentTime > lastTimeInBatch) {
                     pastRequests.push(requestBatchArray);
                 } else if (requestBatchArray[0].approved) {
                     approvedRequests.push(requestBatchArray);
@@ -57,6 +51,7 @@ class RequestExpanderCollection extends Component {
         };
     }
 
+    // Returns an array with unique batch_of_request_id
     filterUniqueBatchIDs = (requests) => {
         let uniqueObject = {};
         for (let request of requests) {
@@ -64,9 +59,27 @@ class RequestExpanderCollection extends Component {
         }
         let uniqueArray = [];
         for (let id in uniqueObject) {
-            uniqueArray.push(id);
+            uniqueArray.push(parseInt(id));
         }
         return uniqueArray;
+    }
+
+    // Return a Moment.js object corresponding to the latest date in an array of
+    // requests.
+    getLastMoment = (requests) => {
+        // Read in an array of objects with the attribute 'date:' and convert
+        // to an array of class 'moment' from Moment.js.
+        let momentArray = [];
+        for (let element of requests) {
+            momentArray.push(moment(element.date));
+        }
+
+        // Sort the array of moments
+        momentArray.sort((left, right) => {
+            return left.diff(right);
+        });
+
+        return momentArray[momentArray.length - 1];
     }
 
     // Show this component on the DOM
