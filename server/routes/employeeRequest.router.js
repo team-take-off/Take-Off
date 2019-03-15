3534ba5811b645e0337834ea821252e334b3a422
Maxwell Todd <mtmaxtodd@gmail.com>
2019-03-15 16:07:38 -0500

    Added functionality to employee requests page to view requests of an employee based on the year they select.

--- server/routes/employeeRequest.router.js
+++ server/routes/employeeRequest.router.js
@@ -8,12 +8,20 @@ router.get('/', (req, res) => {
     if (req.isAuthenticated()) {
         const queryText = `
         SELECT
-            "batch_of_requests".*, "time_off_request".* 
+            "batch_of_requests".*, "time_off_request".* , "employee"."first_name", "employee"."last_name", "type"."name" AS "type"
             FROM "employee" 
             JOIN "batch_of_requests" ON "employee"."id" = "batch_of_requests"."employee_id"
+            JOIN "type" ON "type".id = "batch_of_requests"."type_id"
             JOIN "time_off_request" ON "batch_of_requests"."id" = "time_off_request"."batch_of_requests_id"
             WHERE "employee"."id" = $1;
         `;
+
+        // (`SELECT "time_off_request".*, "batch_of_requests"."date_requested", "batch_of_requests"."approved", "employee"."first_name", "type"."name"
+        //             FROM "employee" 
+        //             JOIN "batch_of_requests" ON "employee"."id" = "batch_of_requests"."employee_id"
+        //             JOIN "type" ON "type".id = "batch_of_requests"."type_id"
+        //             JOIN "time_off_request" ON "batch_of_requests"."id" = "time_off_request"."batch_of_requests_id"
+        //             ;`)
         pool.query(queryText, [req.user.id]).then((queryResponse) => {
             res.send(queryResponse.rows);
         }).catch((queryError) => {
