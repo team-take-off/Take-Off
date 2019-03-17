b1061fb4e91ce66c8d3879756a2bffbc46959aa2
Sharmarke Duale <sduale01@gmail.com>
2019-03-17 17:40:36 -0500

    add Big Calendar

--- src/components/AdminCalendarPage/AdminCalendarPage.js
+++ src/components/AdminCalendarPage/AdminCalendarPage.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+moment.locale('en-US');
+BigCalendar.momentLocalizer(moment);
 class AdminCalendarPage extends Component {
 
     // Show this component on the DOM
     render() {
         return (
             <div>
-                <p>[ AdminCalendarPage ]</p>
+                <p>[ AdminCalenda  rPage ]</p>
             </div>
         );
     }
