d0636996437a917ed531b73355e66a5f71190a02
mes32 <stockman.mike@gmail.com>
2019-03-18 01:46:24 -0500

    Improve CSS styling particularly for table on AdminEmployeeListPage.

--- src/components/AdminEmployeeListPage/AdminEmployeeListPage.js
+++ src/components/AdminEmployeeListPage/AdminEmployeeListPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import './AdminEmployeeListPage.css';
 import EmployeeListRow from './EmployeeListRow/EmployeeListRow';
 
 class AdminEmployeeListPage extends Component {
@@ -23,26 +24,30 @@ class AdminEmployeeListPage extends Component {
                 <button onClick={this.addEmployee}>
                     Add New Employee
                 </button>
-                <h2>List Employees</h2>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Username</th>
-                            <th>Start Date</th>
-                            <th>Vacation Days</th>
-                            <th>Sick and Safe Days</th>
-                            <th>Edit</th>
-                            <th>Activate/Deactivate</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.props.reduxStore.employees.map((employee, i) =>
-                            <EmployeeListRow key={i} employee={employee} />
-                        )}
-                    </tbody>
-                </table>
+                <div className="employee-list-wrapper">
+                    <div className="employee-list-div">
+                        <h2>List Employees</h2>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Username</th>
+                                    <th>Start Date</th>
+                                    <th>Vacation</th>
+                                    <th>Sick & Safe</th>
+                                    <th>Edit</th>
+                                    <th>Active</th>
+                                    <th>Delete</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {this.props.reduxStore.employees.map((employee, i) =>
+                                    <EmployeeListRow key={i} employee={employee} />
+                                )}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         );
     }
