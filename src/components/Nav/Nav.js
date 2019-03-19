ba3665422798e2e72e83fe908e3184dda5eb4582
mes32 <stockman.mike@gmail.com>
2019-03-19 16:38:39 -0500

    Change indent in Nav.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -11,58 +11,64 @@ import './Nav.css';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles  = {
-  mobile:{
-      '@media (min-width:700px)' : {
-          display: 'none'
-  }},
-  desktop:{
-    '@media (max-width:700px)' : {
-        display: 'none'
-}},
+    mobile: {
+        '@media (min-width:700px)' : {
+            display: 'none'
+        }
+    },
+    desktop:{
+        '@media (max-width:700px)' : {
+            display: 'none'
+        }
+    },
 }
 
 class Nav extends Component {
+    render() {
+        const { classes } = this.props;
 
-  render() {
-    const {classes} = this.props
-  return (  
-    <div className="nav">
-      {/* Runs if the window width is less than 700px */}
-      <div className={classnames(classes.mobile, "menu-icon")}><DynamicDrawer user={this.props.user} /></div>
-      <Link to="/home">
-        <h2 className="nav-title">Take-Off</h2>
-      </Link>
-      <div className="counter-balance"></div>
-      <div className={classnames(classes.desktop, "nav-right")}>
-      
-      {this.props.user.id && (
-       this.props.user.role_id === 1 ? 
-        <>
-          <Link className="nav-link" to="/home">Employee Home</Link>
-          <Link className="nav-link" to="/admin/home">Admin Home</Link>
-          <Link className="nav-link" to="/admin/calendar">Calendar</Link>
-          <Link className="nav-link" to="/admin/list_employees">Manage Employees</Link>
-          <Link className="nav-link" to="/admin/search_employee">Search Employees</Link>
-          <LogOutButton className="nav-link" />
-        </>
-      :
-        <>
-          <Link className="nav-link" to="/home">
-            {this.props.user.id ? 'Home' : 'Login / Register'}
-          </Link>
-          <Link className="nav-link" to="/employee_requests">
-            {this.props.user.id && 'My Requests'}
-          </Link>
-          <LogOutButton className="nav-link"/>
-        </>
-    )}
-    </div>
-  </div>
-);
-  }
+        return (  
+            <div className="nav">
+            {/* Runs if the window width is less than 700px */}
+                <div className={classnames(classes.mobile, "menu-icon")}><DynamicDrawer user={this.props.user} /></div>
+                <Link to="/home">
+                    <h2 className="nav-title">Take-Off</h2>
+                </Link>
+                <div className="counter-balance"></div>
+                <div className={classnames(classes.desktop, "nav-right")}>
+
+                    {this.props.user.id && (
+                        this.props.user.role_id === 1 ? 
+                            <>
+                                <Link className="nav-link" to="/home">Employee Home</Link>
+                                <Link className="nav-link" to="/employee_requests">
+                                    {this.props.user.id && 'My Requests'}
+                                </Link>
+                                <Link className="nav-link" to="/admin/home">Admin Home</Link>
+                                <Link className="nav-link" to="/admin/calendar">Calendar</Link>
+                                <Link className="nav-link" to="/admin/list_employees">Manage Employees</Link>
+                                <Link className="nav-link" to="/admin/search_employee">Search Requests</Link>
+                                <LogOutButton className="nav-link" />
+                            </>
+                        :
+                            <>
+                                <Link className="nav-link" to="/home">
+                                    {this.props.user.id ? 'Home' : 'Login / Register'}
+                                </Link>
+                                <Link className="nav-link" to="/employee_requests">
+                                    {this.props.user.id && 'My Requests'}
+                                </Link>
+                                <LogOutButton className="nav-link"/>
+                            </>
+                    )}
+                </div>
+            </div>
+        );
+    }
 }
-const mapStateToProps = state => ({
-  user: state.user,
+
+const mapReduxStoreToProps = reduxStore => ({
+    user: reduxStore.user,
 });
 
-export default withStyles(styles)(connect(mapStateToProps)(Nav));
+export default withStyles(styles)(connect(mapReduxStoreToProps)(Nav));
