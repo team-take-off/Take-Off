e4eb3cb0e6d435114ae6833d0beda9eda4fe8cd4
Sharmarke Duale <sduale01@gmail.com>
2019-03-22 09:44:38 -0500

    test new year vacation rules

---
+++ server/modules/testCronVacationRules.router.js
@@ -0,0 +1,17 @@
+// Vacation Rules
+// PTO = vacation
+// Paid Time of For Vacation and Other Uses
+
+// -Employees can carry over a maxiumum of 5 days of PTO.
+// - January 1, employees get half of their PTO for the year.
+// - June 1, employees get the second half of their PTO for the year.
+// - if employee is hired between october 1 and december 31, they wont get
+//     PTO until january 1.
+// - if employee is hired between january 2 and september 30, they will
+//     get 15 PTO days.
+
+// - 1st year employees earn a total of 15 PTO days.
+// - 2nd year employees earn a total of 18 PTO days.
+// - 3rd-10th year employees earn a total of 20 PTO days.
+// - 11th-13th year employees earn a total of 22 PTO days.
+// - 14th-∞ year employees earn a total of 25 PTO days.
--- server/routes/cronAddLeave.router.js
+++ server/routes/cronAddLeave.router.js
@@ -5,6 +5,54 @@ const cron = require('node-cron');
 let task;
 let currentTime;
 
+// Vacation Rules
+// PTO = vacation
+// Paid Time of For Vacation and Other Uses
+
+// -Employees can carry over a maxiumum of 5 days of PTO.
+// - January 1, employees get half of their PTO for the year.
+// - June 1, employees get the second half of their PTO for the year.
+// - if employee is hired between october 1 and december 31, they wont get
+//     PTO until january 1.
+// - if employee is hired between january 2 and september 30, they will
+//     get 15 PTO days.
+
+// - 1st year employees earn a total of 15 PTO days.
+// - 2nd year employees earn a total of 18 PTO days.
+// - 3rd-10th year employees earn a total of 20 PTO days.
+// - 11th-13th year employees earn a total of 22 PTO days.
+// - 14th-∞ year employees earn a total of 25 PTO days.
+
+const queryText = `SELECT * FROM "employee"`;
+pool.query(queryText).then(response => {
+    // console.log('all employees:', response.rows);
+    response.rows.map(person => {
+        // will grab all of employees vacation hours
+        console.log('employee vacation', person.vacation_hours);
+        let allowedCarryOverHours = 50
+        while(person.vacation_hours > allowedCarryOverHours) {
+            pool.query(`UPDATE "employee" SET "vacation_hours" = "vacation_hours" -1`)
+            .then(response => console.log(response.rows)
+            )
+        }
+    });
+});
+
+// testCronTask = () => {
+//     console.log('hghg');
+    
+//     task = cron.schedule("*/30 * * * *", () => {
+//         // if (task) {
+//         //     task.stop();
+//         // }
+//         // add queries below
+//         console.log('Runs every 30 minutes');
+//         const queryText = `UPDATE "employee" 
+//                         SET "vacation_hours" = "vacation_hours" + 8;`
+//         pool.query(queryText).then(response => console.log(response.rows));
+//     })
+// }
+
 testCronTask = () => {
     console.log('hghg');
     
@@ -19,6 +67,7 @@ testCronTask = () => {
         pool.query(queryText).then(response => console.log(response.rows));
     })
 }
+
 
 testCronTask();
 module.exports = router;
