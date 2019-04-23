c9197a76b4b11aef18327fbe91c980ea0105f2b9
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2019-04-23 14:25:14 -0500

    edit dialog saving previous values in state

--- src/components/RequestCard/EditDialog.js
+++ src/components/RequestCard/EditDialog.js
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { connect } from 'react-redux';
 import { Dialog } from '@material-ui/core';
+import swal from 'sweetalert';
+const moment = require('moment-business-days');
 
 class EditDialog extends Component {
 
+  state = {
+    startDate: '',
+    startDateType: 8,
+    endDate: '',
+    endDateType: 8,
+  }
+
+  componentWillReceiveProps(props) {
+    if(props.open) {
+      let startEntry = props.startingArray[0];
+      let endEntry = props.startingArray[props.startingArray.length - 1];
+      let sDateVal = moment(startEntry.date).format('YYYY-MM-DD');
+      let eDateVal = moment(endEntry.date).format('YYYY-MM-DD');
+      this.setState({
+        startDate: sDateVal,
+        startDateType: startEntry.hours,
+        endDate: eDateVal,
+        endDateType: endEntry.hours,
+      });
+    }
+  }
+
+  renderStartDate = () => {
+    return (
+      <div>
+
+      </div>
+    )
+  }
 
   handleCommit = () => {
     this.props.closeEdit();
@@ -18,7 +48,15 @@ class EditDialog extends Component {
     return (
       <Dialog open={this.props.open}>
         <div className="inner-dialog">
-          {JSON.stringify(this.props.startingArray, null, 2)}
+          <div>
+            <p>Start Date: {this.state.startDate}</p>
+            <p>{this.state.startDateType === 8? 'Full Day' : 'Half Day'}</p>
+          </div>
+          <div>
+            <p>End Date: {this.state.endDate}</p>
+            <p>{this.state.endDateType === 8? 'Full Day' : 'Half Day'}</p>
+          </div>
+          {this.renderStartDate()}
           <button onClick={this.handleCommit}>Commit</button>
           <button onClick={this.handleCancel}>Cancel</button>
         </div>
