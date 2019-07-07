c3df80eaaf43697ecc6adc03a42bb894c6bfa0d4
mes32 <stockman.mike@gmail.com>
2019-07-06 19:24:08 -0500

    Change database schema to use date ranges, exclude over-lapping requests for the same employee, and log changes

--- database_schema.sql
+++ database_schema.sql
@@ -1,18 +1,16 @@
+DROP TABLE IF EXISTS transaction_log;
 DROP TABLE IF EXISTS time_off_request;
-DROP TABLE IF EXISTS batch_of_requests;
-DROP TABLE IF EXISTS accrued_time;
 DROP TABLE IF EXISTS employee;
 DROP TABLE IF EXISTS employee_role;
 DROP TABLE IF EXISTS leave_type;
 DROP TABLE IF EXISTS request_status;
+DROP TABLE IF EXISTS transaction_type;
 
 -- Differentiate roles between administators and employees
-CREATE TABLE employee_role
-(
-	id SERIAL PRIMARY KEY,
-	title VARCHAR(20) NOT NULL
+CREATE TABLE employee_role (
+	id SERIAL PRIMARY KEY
+	, title VARCHAR(20) NOT NULL
 );
-
 INSERT INTO employee_role
 	(title)
 VALUES
@@ -20,25 +18,21 @@ VALUES
 	('employee');
 
 -- Type of leave
-CREATE TABLE leave_type
-(
-	id SERIAL PRIMARY KEY,
-	val VARCHAR(20) NOT NULL
+CREATE TABLE leave_type (
+	id SERIAL PRIMARY KEY
+	, val VARCHAR(20) NOT NULL
 );
-
 INSERT INTO leave_type
 	(val)
 VALUES
 	('Vacation'),
 	('Sick and Safe Leave');
 
--- Status of a batch of requested time off
-CREATE TABLE request_status
-(
-	id SERIAL PRIMARY KEY,
-	val VARCHAR(20) NOT NULL
+-- Status of a request for time off
+CREATE TABLE request_status (
+	id SERIAL PRIMARY KEY
+	, val VARCHAR(20) NOT NULL
 );
-
 INSERT INTO request_status
 	(val)
 VALUES
@@ -46,46 +40,53 @@ VALUES
 	('approved'),
 	('denied');
 
+-- Types of a transactions in the log table
+CREATE TABLE transaction_type (
+	id SERIAL PRIMARY KEY
+	, val VARCHAR(20) NOT NULL
+);
+INSERT INTO transaction_type
+	(val)
+VALUES
+	('automatic accrual'),
+	('automatic adjustment'),
+	('employee request'),
+	('employee cancel'),
+	('admin approve'),
+	('admin deny'),
+	('admin for employee');
+
 -- Employee data
-CREATE TABLE employee
-(
-	id SERIAL PRIMARY KEY,
-	email VARCHAR(100) UNIQUE NOT NULL,
-	first_name VARCHAR(25) NOT NULL,
-	last_name VARCHAR(25) NOT NULL,
-	sick_hours INTEGER NOT NULL DEFAULT 0,
-	vacation_hours INTEGER NOT NULL DEFAULT 0 CHECK(vacation_hours >= 0),
-	role_id INTEGER REFERENCES employee_role(id) DEFAULT 2,
-	started_date DATE NOT NULL,
-	is_active BOOLEAN DEFAULT true
+CREATE TABLE employee (
+	id SERIAL PRIMARY KEY
+	, email VARCHAR(100) UNIQUE NOT NULL
+	, first_name VARCHAR(25) NOT NULL
+	, last_name VARCHAR(25) NOT NULL
+	, sick_hours INTEGER NOT NULL DEFAULT 0
+	, vacation_hours INTEGER NOT NULL DEFAULT 0 CHECK(vacation_hours >= 0)
+	, role_id INTEGER NOT NULL REFERENCES employee_role(id) DEFAULT 2
+	, started_date DATE NOT NULL
+	, is_active BOOLEAN NOT NULL DEFAULT true
 );
 
--- Keeps track of vacation/sick time earned
-CREATE TABLE accrued_time
-(
-	id SERIAL PRIMARY KEY,
-	date_accrued DATE DEFAULT NOW() NOT NULL,
-	sick_hours INTEGER NOT NULL,
-	vacation_hours INTEGER NOT NULL,
-	employee_id INTEGER REFERENCES employee(id)
+-- An employee's request for time off
+CREATE TABLE time_off_request (
+	id SERIAL PRIMARY KEY
+	, employee_id INTEGER NOT NULL REFERENCES employee(id)
+	, leave_type_id INTEGER NOT NULL REFERENCES leave_type(id)
+	, start_datetime TIMESTAMPTZ NOT NULL
+	, end_datetime TIMESTAMPTZ NOT NULL
+	, placed_datetime TIMESTAMPTZ NOT NULL DEFAULT NOW()
+	, CONSTRAINT end_after_start CHECK(end_datetime > start_datetime)
+	, CONSTRAINT exclude_overlapping_requests EXCLUDE USING gist(int4range(employee_id, employee_id, '[]') WITH =, tstzrange(start_datetime, end_datetime) WITH &&)
 );
 
--- Groups one or more entries in "time_off_request" into a single transaction
-CREATE TABLE batch_of_requests
-(
-	id SERIAL PRIMARY KEY,
-	employee_id INTEGER REFERENCES employee(id),
-	date_requested TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-	leave_type_id INTEGER REFERENCES leave_type(id),
-	request_status_id INTEGER REFERENCES request_status(id) DEFAULT 1,
-	notes VARCHAR(250)
-);
-
--- Stores time off request data
-CREATE TABLE time_off_request
-(
-	id SERIAL PRIMARY KEY,
-	off_date DATE NOT NULL,
-	batch_of_requests_id INTEGER REFERENCES batch_of_requests(id),
-	off_hours INTEGER NOT NULL
-);
+CREATE TABLE transaction_log (
+	id SERIAL PRIMARY KEY
+	, employee_id INTEGER NOT NULL REFERENCES employee(id)
+	, hours_changed INTEGER NOT NULL
+	, leave_type_id INTEGER NOT NULL REFERENCES leave_type(id)
+	, transaction_type_id INTEGER NOT NULL REFERENCES transaction_type(id)
+	, final BOOLEAN NOT NULL
+	, transaction_datetime TIMESTAMPTZ NOT NULL DEFAULT NOW()
+)
