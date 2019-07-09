04f32e068515a79ba83c7e930789177183cf9d3f
mes32 <stockman.mike@gmail.com>
2019-07-08 20:32:15 -0500

    Rename sickRequestDate to enteredSickRequest. Rename vacationRequestDate to enteredVacationRequest

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,13 +1,13 @@
 import { combineReducers } from 'redux';
 import adminMode from './adminMode';
-import errors from './errorsReducer';
-import loginMode from './loginModeReducer';
-import user from './userReducer';
-import requests from './requestsReducer';
-import userRequests from './userRequestsReducer';
-import employees from './employeesReducer';
-import vacationRequestDates from './vacationRequestDatesReducer';
-import sickRequestDates from './sickRequestDatesReducer';
+import employees from './employees';
+import enteredSickRequest from './enteredSickRequest';
+import enteredVacationRequest from './enteredVacationRequest';
+import errors from './errors';
+import loginMode from './loginMode';
+import requests from './requests';
+import user from './user';
+import userRequests from './userRequests';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -17,14 +17,14 @@ import sickRequestDates from './sickRequestDatesReducer';
 // This is what we get when we use 'state' inside of 'mapStateToProps'
 const rootReducer = combineReducers({
   adminMode,
-  errors, // contains registrationMessage and loginMessage
-  loginMode, // will have a value of 'login' or 'registration' to control which screen is shown
-  user, // will have an id and username if someone is logged in
-  requests,
-  userRequests,
   employees,
-  vacationRequestDates,
-  sickRequestDates
+  enteredSickRequest,
+  enteredVacationRequest,
+  errors,
+  loginMode,
+  requests,
+  user,
+  userRequests,
 });
 
 export default rootReducer;
